bd4c62afdf2c5881df05633ea68a1945dd89fc1c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-05 13:46:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,3 +16,5 @@ create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta li a span.location-name").text
