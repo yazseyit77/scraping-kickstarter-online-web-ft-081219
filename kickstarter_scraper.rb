18ac905efc30448d00c56fac3631bc1bf413a208
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-05 13:53:29 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,12 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+
   binding.pry
 end
 
